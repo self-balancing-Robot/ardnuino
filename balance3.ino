0bd04125d33cbb0bc16e21491dfcad37acdce18b
yusufso <y.hussain123@outlook.com>
2019-12-02 13:46:05 +0000

    added integral control so now theres PID

--- balance3.ino
+++ balance3.ino
@@ -1,10 +1,10 @@
 
-//PD controller constants
+//PID controller constants
 float Kp = 1.0;
 float Kd = 1.0;
+float Ki = 1.0;
 
-
-//setPoint is 0 degrees, fl
+//setPoint is 0 degrees, 511 is the analog bit equivalent
 int setPoint = 511;
   
 #define pinWheelR1 8
@@ -23,9 +23,22 @@ int setPoint = 511;
 //  R1: HIGH R2: LOW
 //  L1: LOW L2: HIGH
 
-float speedMotor = 50;
 
-float lastAngle = 0;
+//To keep track of area by using trapzeium rule so for 
+//every loop iteration(50ms), get the iteration's 
+//trapezium area and add this to previousArea.
+//This is for the integral part of PID control
+int previousArea = 0;
+int previousError = 0;
+//To get the derivative by getting the gradient from
+//this variable to the current measured angle
+//This is for the derivative part of the PID control
+int lastAngle = 0;
+
+
+//Time between each measurement and correction
+int intervalTime = 50;
+
 void setup() {
   //pin setup
   pinMode(pinWheelR1, OUTPUT);
@@ -44,27 +57,43 @@ void setup() {
 
 void loop() {
   //get angle from pinGyroAngle
-  float analogAngle = analogRead(pinGyroAngle);
+  int analogAngle = analogRead(pinGyroAngle);
   //0 to 510 bits is a forward tilt
   //511 is vertical
   //512 to 1023 bits is a backward tilt
 
-  //ENABLER
-  //analogWrite(pinWheelREnable, 255);
-  //analogWrite(pinWheelLEnable, 255);
   
   //getting error from feedback
   //positive if angle > 511 (forward tilt)
   //negative if angle < 511 (backward tilt) 
-  float error = setPoint - analogAngle;
-  //applying Kp and Kd
-  //correctionAngle = error * Kp + d/dt (error) * Kd
-  float correction = error * Kp + ((error - lastAngle)/50) * Kd;
-  //refresh lastAngle
+  int error = setPoint - analogAngle;
+  
+  //applying Kp, Ki and Kd
+  //current error * Kp
+  int pCorrection = error * Kp;
+  //current interval's trapezium area * Ki
+  int iTrapeziumArea = 0.5 * (previousError + error) * intervalTime; 
+  int iCorrection = iTrapeziumArea * Ki;
+  //current interval's gradient * kd
+  int dDeriv = ((error - lastAngle)/intervalTime);
+  int dCorrection = dDeriv * Kd;
+
+  //Adding individual P, I and D corrections to total correction
+  int correction= pCorrection + iCorrection + dCorrection;
+  
+  //refresh variables
+  previousArea = previousArea + iTrapeziumArea;
+  previousError = error;
   lastAngle = analogAngle;
    
 
-     
+  //correction is positive/negative to deduce the direction
+  //of motor rotation.
+  //We don't need that as we use analogAngle to dictate the direction.
+  //Therefore correctionAngle is only needed for its magnitude
+  //so we will get its absolute value.
+  correction = abs(correction);
+  
   //if it is tilting forward(neg correctionAngle), go forward
   if (analogAngle < 511){
     Serial.println("Moving forward");
@@ -73,9 +102,6 @@ void loop() {
     digitalWrite(pinWheelL1, HIGH);
     digitalWrite(pinWheelL2, LOW);
 
-    //get correctionAngle as int
-    correction = round(correction);
-    correction = abs(correction);
     //setMotorSpeed(correctionAngle)
     analogWrite(pinWheelREnable, correction);
     analogWrite(pinWheelLEnable, correction);
@@ -88,11 +114,8 @@ void loop() {
     digitalWrite(pinWheelL1, LOW);
     digitalWrite(pinWheelL2, HIGH);
 
-    //get correctionAngle as int
-    correction = round(correction);
-    correction = abs(correction);
     //setMotorSpeed(correctionAngle)
-    analogWrite(pinWheelREnable,correction);
+    analogWrite(pinWheelREnable, correction);
     analogWrite(pinWheelLEnable, correction);
   }
   //if it's balanced, leave it
@@ -107,5 +130,5 @@ void loop() {
     analogWrite(pinWheelLEnable, 0);
   }
 
-  delay(50);
+  delay(intervalTime);
 }
